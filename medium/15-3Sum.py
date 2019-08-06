adcd1253ca18bfd4d01c7f2a65413a5d17548126
Henry <47991972+chonhenry@users.noreply.github.com>
2019-08-05 22:46:57 -0400

    Update 15-3Sum.py

--- medium/15-3Sum.py
+++ medium/15-3Sum.py
@@ -1,31 +1,38 @@
 nums = [-1,0,1,2,-1,-4,-2,-3,3]
+#sorted: [-4, -3, -2, -1, -1, 0, 1, 2, 3]
 
 def threeSum(nums):
   nums.sort()
+  print('sorted:', nums)
+  print('******************************************************')
   arr = []
-  for i in range(len(nums)):
+  for i in range(len(nums)-1):
     diff = 0-nums[i]
-    self.helper(diff, nums[i+1:], arr)
-    print(arr,'============================')
+    if nums[:i].count(nums[i])==0:
+      helper(diff, nums[i+1:], arr)
+    #print('arr:', arr)
+    #print('=================================================')
+  return arr
             
-def helper(self, diff, sub_nums, arr):
+def helper(diff, sub_nums, arr):
   maximum = len(sub_nums)-1
   minimum = 0
-        
-  while (maximum != minimum):
-    print(sub_nums)
-    print('min ', minimum)
-    print('max ', maximum)
+  #print('diff:',diff)
+  while (maximum > minimum):
+    #print('sub_nums:', sub_nums, '  min:', minimum, '  max:', maximum)
     s = sub_nums[minimum]+sub_nums[maximum]
     if s==diff:
-      arr.append([-1*diff, sub_nums[minimum], sub_nums[maximum]])
+      tmp = [-1*diff, sub_nums[minimum], sub_nums[maximum]]
+      if tmp not in arr:
+        arr.append(tmp)
       minimum+=1
       maximum-=1
     elif s < diff:
       minimum+=1
     else:
       maximum-=1
-            
-        
+
+
+print(threeSum(nums))
             
         
